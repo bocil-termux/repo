77e94d374ce409d85b81e119b6e35dd18ac440d3
bocil-termux <pentesting833@gmail.com>
2025-09-06 03:45:55 +0800

    Add files via upload

---
+++ file/mulai.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main() {
+    char *python_script = NULL;
+    char *python_cmd = NULL;
+    if (access("/data/data/com.termux/files/usr", F_OK) == 0) {
+        python_script = "/data/data/com.termux/files/usr/bin/mulai.py";
+        python_cmd = "python";
+    } else {
+        python_script = "/usr/bin/mulai.py";
+        python_cmd = "python3";
+    }
+    if (access(python_script, F_OK) != 0) {
+        fprintf(stderr, "Error: File %s tidak ditemukan.\n", python_script);
+        return 1;
+    }
+    execlp(python_cmd, python_cmd, python_script, NULL);
+    perror("Error menjalankan perintah");
+    return 1;
+}
